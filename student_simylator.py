bdb5e680c6f517c0c0a04de98778d93088feadb9
Matyuk-Maxym <maks.matuk17@gmail.com>
2024-09-14 14:35:39 +0300

    save student

--- student_simylator.py
+++ student_simylator.py
@@ -7,6 +7,7 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 10
         self.alive = True
 
     def to_study(self):
@@ -23,8 +24,16 @@ class Student:
     def to_chill(self):
 
         print("Rest time")
-        self.gladness += 5
-        self.progress -= 0.1
+        self.gladness += 2
+        self.progress -= 0.4
+        self.money -= 2
+
+    def to_work(self):
+
+        print("Time to work")
+        self.progress += 0.3
+        self.gladness -= 0.3
+        self.money += 2
 
 
     def is_alive(self):
@@ -34,25 +43,31 @@ class Student:
         elif self.gladness <= 0:
             print("Depression…")
             self.alive = False
-        elif self.progress > 5:
+        elif self.progress > 10:
             print("Passed externally…")
+            self.alive = False
+        elif self.money <= 0:
+            print("No money")
             self.alive = False
 
     def end_of_day(self):
 
         print(f"Gladness = {self.gladness}")
         print(f"Progress = {round(self.progress, 2)}")
+        print(f"Money = {self.money}")
 
     def live(self, day):
 
         day = "Day" + str(day) + "of" +self.name + "life"
         print(f"{day:=^50}")
-        live_cube = random.randint(1, 3)
+        live_cube = random.randint(1, 4)
         if live_cube == 1:
             self.to_study()
         elif live_cube == 2:
             self.to_sleep()
-        elif live_cube == 3:
+        elif self.to_work() == 3:
+            self.to_work()
+        elif live_cube == 4:
             self.to_chill()
             self.end_of_day()
             self.is_alive()
