71f943694f9fbe437056c975252c1b75b559a63d
Matyuk-Maxym <maks.matuk17@gmail.com>
2024-10-06 11:54:05 +0300

    save

---
+++ lesson and dz 10/lesson 10.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect('itstep_DB.sl3', 5)
+cursor = connection.cursor()
+
+cursor.execute('create table users(name text, password text);')
+
+connection.commit()
+
+# print(connection)
+# print(cursor)
+
+connection.close()
