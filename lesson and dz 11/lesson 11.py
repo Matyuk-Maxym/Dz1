4885383938962440c1385ecc30489d51a0bc70f5
Matyuk-Maxym <maks.matuk17@gmail.com>
2024-10-13 11:51:34 +0300

    save

--- lesson and dz 11/lesson 11.py
+++ lesson and dz 11/lesson 11.py
@@ -1,5 +1,5 @@
 import cv2
-
+from PIL import Image
 
 
 image_path = 'cat.jpeg'
@@ -9,6 +9,12 @@ image = cv2.imread(image_path)
 cat_face = cat_face_cascade.detectMultiScale(image)
 
 
-print(cat_face)
-cv2.imshow("Cat", image)
-cv2.waitKey()
+cat = Image.open(image_path)
+glasses = Image.open('glasses.png')
+for (x,y,w,h) in cat_face:
+    glasses = glasses.resize((w, int(h / 3)))
+    cat.paste(glasses, (x, int(y + h / 4)),glasses)
+    cat.save("cat_with_glasses.png")
+    cat_with_glasses = cv2.imread("cat_with_glasses.png")
+    cv2.imshow("Cat_with_glasses", cat_with_glasses)
+    cv2.waitKey()
